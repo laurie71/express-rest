e690e45cfa8459cc8c7e8f771518b9a0bd6c3b35
Laurie Harper <laurie@holoweb.net>
2011-02-12 19:30:06 -0500

    Cleanup API doc generator script

--- lib/mkapi.sh
+++ lib/mkapi.sh
@@ -1,6 +1,6 @@
-#JSDOCDIR=../site/_tools/jsdoc-toolkit $JSDOCDIR/jsrun.sh -a -r \
-#	-t=../site/_jsdoc/templates -d=../site/api \
-#	express-rest
-JSDOCDIR=../site/_tools/jsdoc-toolkit $JSDOCDIR/jsrun.sh -a -r \
-	-t=$JSDOCDIR/templates/jsdoc -d=../site/api \
-	express-rest
+JSDOCDIR=../tools/jsdoc-toolkit
+#TEMPLATE=../site/_jsdoc/templates
+TEMPLATE=$JSDOCDIR/templates/jsdoc
+
+$JSDOCDIR/jsrun.sh -a -r -t=$TEMPLATE -d=../site/api express-rest
+
